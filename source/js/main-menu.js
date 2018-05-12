57cd393da18cba7263b50d1ea6e5637cca831c59
Sergey Ostapovec <seraphimaion@gmail.com>
2018-05-12 20:00:00 +0300

    fixes

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -3,10 +3,12 @@
   var menuList = document.querySelector('.main-menu__list');
   var menuList2 = document.querySelector('.main-menu__list2');
   var menuToggle = document.querySelector('.main-menu__toogle');
+
   menuList.classList.add('main-menu__list--no-js');
   menuList2.classList.add('main-menu__list--no-js');
   menuToggle.classList.remove('main-menu__toogle--close');
   menuToggle.classList.add('main-menu__toogle--open');
+
   menuToggle.addEventListener('click', function () {
     if (menuToggle.classList.contains('main-menu__toogle--open')) {
       menuToggle.classList.remove('main-menu__toogle--open');
